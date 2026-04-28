9a3068b8d7cf169e9c500c2f21ad5260246005cb
Muhammad Ahmad <m.ahmadmalik03@gmail.com>
2026-04-28 23:50:33 +0500

    Update analysis.py File

--- analysis.py
+++ analysis.py
@@ -18,8 +18,29 @@ def load_stock_data(filepath):
         'volumes':volumes
     }
 def calculate_statistics(stock_data):
-    pass
+    closes=stock_data['closes']
+    volumes=stock_data['volumes']
+    
+    avg_price=np.mean(closes)
+    max_price=np.max(closes)
+    min_price=np.min(closes)
+    std_dev=np.std(closes)
 
+    price_change=closes[-1]-closes[0]
+    percent_change=(price_change/closes[0])*100
+
+    avg_volume=np.mean(volumes)
+    total_volume=np.sum(volumes)
+    return{
+        'avg_price':avg_price,
+        'max_price':max_price,
+        'min_price':min_price,
+        'std_dev':std_dev,
+        'price_change':price_change,
+        'percent_change':percent_change,
+        'avg_volume':avg_volume,
+        'total_volume':total_volume
+    }
 def find_best_worst_days(stock_data):
     pass
 
