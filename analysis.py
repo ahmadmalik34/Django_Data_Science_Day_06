8d1b9a9ad6cf24aa5d9adfe9bf2db62f67c3ee6e
Muhammad Ahmad <m.ahmadmalik03@gmail.com>
2026-04-28 23:39:27 +0500

    Update analysis.py File

--- analysis.py
+++ analysis.py
@@ -4,3 +4,16 @@ from datetime import datetime
 def load_stock_data(filepath):
     data=np.genfromtxt(filepath,delimeter=',',skip_header=1)
     dates=data[:,0]
+    open=data[:,1]
+    highs=data[:,2]
+    lows=data[:,3]
+    closes=data[:,4]
+    volumes=data[:,5]
+    return{
+        'dates':dates,
+        'open':open,
+        'highs':highs,
+        'lows':lows,
+        'closes':closes,
+        'volumes':volumes
+    }
